b738a73e90c64212f45b09b93030bc100d8e9f34
suraj <suraj.bm@eazylms.com>
2025-02-10 15:31:06 +0530

    req

--- project/project/settings.py
+++ project/project/settings.py
@@ -85,7 +85,7 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default':  dj_database_url.config(default='sqlite:///db.sqlite3')
+   'default': dj_database_url.config(default=os.getenv("DATABASE_URL"))
 }
 
 
